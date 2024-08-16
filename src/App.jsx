2838e2de7007c76e16f81089a607a243adab8ea5
Yenner Alayon <yenneralayon@hotmail.com>
2024-08-15 21:01:23 -0500

    feat(front):Add accesibility menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {Profile} from './components/profile.jsx'
 import {Portfolio} from './components/portfolio.jsx' 
 import {Contact} from './components/contact.jsx'
 import {Footer} from './components/footer.jsx'
-import {ColorSample} from './components/colorSample.jsx';
 import {AccessibilityControls} from './components/accesibityControls.jsx'
 import {useFontSizeChange} from './Hooks/useFontsizeChange.js'
 import {useContrastChange} from './Hooks/useContrastChange.js'
@@ -16,13 +15,12 @@ const App = () => {
   useFontSizeChange()
   useContrastChange()
   useColorBlindnessChange()
-  useReadText()
+  //useReadText()
   
   return (
     <>
         <Header/>
         <AccessibilityControls />
-        <ColorSample/>
         <Profile />
         <Portfolio />
         <Contact />
--- src/components/accesibityControls.jsx
+++ src/components/accesibityControls.jsx
@@ -1,38 +1,54 @@
-import PropTypes from 'prop-types';
-
-// Componente Funcional
-
-export const AccessibilityControls = ({ fontSizeRef, contrastRef, colorBlindnessRef}) => (
-  <div className="accessibility-controls">
-    <label htmlFor="font-size">Tamaño de fuente:</label>
-    <select id="font-size" ref={fontSizeRef}>
-      <option value="default">Por defecto</option>
-      <option value="large">Grande</option>
-      <option value="x-large">Muy grande</option>
-    </select>
-
-    <label htmlFor="contrast">Contraste:</label>
-    <select id="contrast" ref={contrastRef}>
-      <option value="default">Por defecto</option>
-      <option value="high-contrast">Alto contraste</option>
-    </select>
-
-    <label htmlFor="color-blindness">Filtros para daltonismo:</label>
-    <select id="color-blindness" ref={colorBlindnessRef}>
-      <option value="default">Por defecto</option>
-      <option value="protanopia">Protanopia</option>
-      <option value="deuteranopia">Deuteranopia</option>
-      <option value="tritanopia">Tritanopia</option>
-    </select>
-    </div>
-);
-
-// En este código, se usa PropTypes.shape para definir la forma del objeto que se espera para cada ref.
-// Cada ref debe ser un objeto con una propiedad current que es una instancia de Element. 
-// Esto garantiza que los props pasados al componente tengan la forma esperada.
-AccessibilityControls.propTypes = {
-  fontSizeRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-  contrastRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-  colorBlindnessRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-  voiceSelectRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-};
+export const AccessibilityControls = () => {
+  return(
+      <aside className="sidebar">
+        <form action="" className="sidebar__form">
+            <input type="checkbox" id="open-menu"/>
+            <label htmlFor="open-menu" className="material-symbols-outlined close-button">close</label>
+            <label htmlFor="open-menu" className="material-symbols-outlined open-button">double_arrow</label>
+        </form>
+        <h1 className="sidebar__tittle">
+          Menú de Accesibilidad 
+        </h1>
+        <nav className="sidebar__nav">
+            <ul>
+              <div className="sidebar__container">
+                    <li className="sidebar__item">
+                        <a href="">
+                        <span className="material-symbols-outlined">text_fields</span><br />
+                        Tamaño texto
+                        </a>
+                    </li>
+                    <li className="sidebar__item">
+                          <a href="">
+                              <span className="material-symbols-outlined">contrast</span>
+                              Contraste
+                          </a>          
+                    </li>
+              </div>
+              <div className="sidebar__container">
+                  <li className="sidebar__item">
+                        <a href="">
+                            <span className="material-symbols-outlined">text_to_speech</span>
+                            Lector Pantalla
+                        </a>
+                    </li>
+                    <li className="sidebar__item">
+                        <a href="#">
+                          <span className="material-symbols-outlined">web_traffic</span>
+                          Cursor
+                        </a>
+                    </li>
+              </div>  
+            </ul>
+        </nav>
+        <div className="sidebar_profile">
+                  <ul>
+                     <li className="sidebar__item  button-end">
+                          <span class="material-symbols-outlined">restart_alt</span>
+                          <a href="#">Restablecer configuraciones</a>
+                     </li>
+                  </ul>
+        </div>
+    </aside>
+  )
+}
