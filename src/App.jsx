63dbf46d779b1b74b74a922be6128351a9943da4
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-08-15 19:18:00 -0500

    feat(router): add router hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useContrastChange } from './Hooks/useContrastChange.js';
 import { useColorBlindnessChange } from './Hooks/useColorBlindnessChange.js';
 import { useReadText } from './Hooks/useReadText.js';
 
+import UseDocumentTitleUpdater from './Hooks/useDocumentTitleUpdater.js';
+
 import Home from './pages/index';
 import Error from './pages/error';
 import Wcag from './pages/wcag';
@@ -13,37 +15,6 @@ import Docs from './pages/docs';
 import Developer from './pages/developer';
 import Designer from './pages/designer';
 import AppLayout from './layouts/app';
-
-// Componente que gestiona el título del documento
-function DocumentTitleUpdater() {
-    const location = useLocation();
-
-    useEffect(() => {
-        // Cambia el título de la página según la ruta actual
-        switch (location.pathname) {
-            case '/':
-                document.title = 'Inicio | Web Accesible';
-                break;
-            case '/wcag':
-                document.title = 'Enfermedades y WCAG | Web Accesible';
-                break;
-            case '/docs':
-                document.title = 'Documentación | Web Accesible';
-                break;
-            case '/docs/developer':
-                document.title = 'Documentación para Desarrolladores | Web Accesible';
-                break;
-            case '/docs/designer':
-                document.title = 'Documentación para Diseñadores | Web Accesible';
-                break;
-            default:
-                document.title = 'Error | Web Accesible';
-                break;
-        }
-    }, [location.pathname]);
-
-    return null;
-}
 
 export default function App() {
 
@@ -54,7 +25,7 @@ export default function App() {
 
     return (
         <BrowserRouter>
-            <DocumentTitleUpdater />
+            <UseDocumentTitleUpdater />
             <Routes>
                 <Route path="/" element={<AppLayout />}>
                     <Route index element={<Home />} />
