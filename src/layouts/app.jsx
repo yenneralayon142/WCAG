d59b51dc7ada04c0c036e618aee622888b556c65
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-08-17 02:12:10 -0500

    styles(sidebar): add sidebar in the layout, fix position and add animation

--- src/layouts/app.jsx
+++ src/layouts/app.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 import Footer from "./footer";
+import AccessibilityControls from "./accesibityControls";
 
 export default function AppLayout() {
     return (
         <div id="app__container">
+            <AccessibilityControls />
             <Header />
             <Outlet />
             <Footer />
