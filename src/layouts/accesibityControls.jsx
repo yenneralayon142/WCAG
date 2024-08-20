f2cbbf8f34613face340fff83fa0b92407dd7eca
Yenner Alayon <yenneralayon@hotmail.com>
2024-08-20 14:34:13 -0500

    fix(front): Resolving bug in accesibility menu

--- src/layouts/accesibityControls.jsx
+++ src/layouts/accesibityControls.jsx
@@ -1,7 +1,14 @@
+
 import { Button } from "@progress/kendo-react-buttons";
 import * as icons from "@progress/kendo-svg-icons";
 
 export default function AccessibilityControls() {
+    const handleMoveWidget = () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.classList.toggle('sidebar-right');
+    };
+
+    
     return (
         <form>
             <input type="checkbox" id="open-menu" className="hidden" aria-label="Abrir menú de accesibilidad web"/>
@@ -20,49 +27,36 @@ export default function AccessibilityControls() {
             <div className="overlay" onClick={() => document.getElementById('open-menu').click()}></div>
             <div className="sidebar">
                 <div className="sidebar__tittle">
-                    <h1 className="text-2xl">
-                        Menú de Accesibilidad
-                    </h1>
-                    <h2 className="text-lg">
-                        Aumentar tamaño Widgets
-                    </h2>
+                    <h1 className="text-2xl">Menú de Accesibilidad</h1>
+                    <h2 className="text-lg">Aumentar tamaño Widgets</h2>
                 </div>
                 <div className="sidebar__container">
                     <article className="sidebar__item">
                         <a href="">
-                            <span className="material-symbols-outlined">
-                                text_fields
-                            </span>
+                            <span className="material-symbols-outlined">text_fields</span>
                             <br />
                             Tamaño texto
                         </a>
                     </article>
                     <article className="sidebar__item">
                         <a href="">
-                            <span className="material-symbols-outlined">
-                                contrast
-                            </span>
+                            <span className="material-symbols-outlined">contrast</span>
                             Contraste
                         </a>
                     </article>
                     <article className="sidebar__item">
                         <a href="">
-                            <span className="material-symbols-outlined">
-                                text_to_speech
-                            </span>
+                            <span className="material-symbols-outlined">text_to_speech</span>
                             Lector Pantalla
                         </a>
                     </article>
                     <article className="sidebar__item">
                         <a href="">
-                            <span className="material-symbols-outlined">
-                                web_traffic
-                            </span>
+                            <span className="material-symbols-outlined">web_traffic</span>
                             Cursor
                         </a>
                     </article>
                 </div>
-                
                 <div className="sidebar__buttons">
                     <label htmlFor="open-menu" className="close-button" aria-label="Cerrar menú de accesibilidad web">
                         <Button
@@ -70,7 +64,6 @@ export default function AccessibilityControls() {
                             fillMode="flat"
                             svgIcon={icons.xIcon}
                             size="large"
-                            htmlFor="open-menu"
                             type="button"
                             style={{width: '100%'}}
                             onClick={() => document.getElementById('open-menu').click()}
@@ -78,17 +71,18 @@ export default function AccessibilityControls() {
                             Cerrar
                         </Button>
                     </label>
-                    <Button
-                        rounded="full"
-                        fillMode="flat"
-                        svgIcon={icons.arrowRotateCcwIcon}
-                        size="large"
-                        type="button"
-                        className="button-end"
-                        style={{width: '100%'}}
-                    >
-                        Restablecer configuraciones
-                    </Button>
+                    <label className="change-position" aria-label="Cambiar de posición menú de accesibilidad" onClick={handleMoveWidget}>
+                        <Button
+                            rounded="full"
+                            fillMode="flat"
+                            svgIcon={icons.fileConfigIcon}
+                            size="large"
+                            type="button"
+                            style={{width: '100%'}}
+                        >
+                            Mover Widget
+                        </Button>
+                    </label>
                 </div>
             </div>
         </form>
