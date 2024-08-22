078fd647a32557c507fe728a0d0ed0aa0c2e8734
Yenner Alayon <yenneralayon@hotmail.com>
2024-08-22 14:43:00 -0500

    feat(front): responsive inicio

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -16,6 +16,26 @@ export default function Index(){
                             <p>Esta página facilita a los programadores/diseñadores web la creación de sitios que cumplan con los estándares WCAG, garantizando la accesibilidad para todos los usuarios, incluidas personas con discapacidades.</p>
                         </div>
                 </div>
+                <section className="services">
+                        <h2 className="services__title">¿Qué ofrecemos?</h2>
+                        <div className="services__container">
+                            <div className="service">
+                                <img src="../../public/gestion-de-la-informacion.png" alt="Icono de Información" className="service__icon"/>
+                                <h3 className="service__title">Información</h3>
+                                <p className="service__description">Relacionada con las directrices WCAG</p>
+                            </div>
+                            <div className="service">
+                                <img src="../../public/expediente1.png" alt="Icono de Documentación" className="service__icon"/>
+                                <h3 className="service__title">Documentación</h3>
+                                <p className="service__description">Guía para una mejor comprensión</p>
+                            </div>
+                            <div className="service">
+                                <img src="codificacion2.png" alt="Icono de HTML/CSS" className="service__icon"/>
+                                <h3 className="service__title">HTML / CSS</h3>
+                                <p className="service__description">Modelos para programar fácilmente</p>
+                            </div>
+                        </div>
+                </section>
             </main>
         </>
     );
