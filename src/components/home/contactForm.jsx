945448aaeb2d805a15bd25382bec22c7ce961580
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-08-23 00:21:22 -0500

    style(front): add new css file and finish contact form

--- src/components/home/contactForm.jsx
+++ src/components/home/contactForm.jsx
@@ -1,31 +1,37 @@
+import { Button } from "@progress/kendo-react-buttons";
+import * as icons from "@progress/kendo-svg-icons";
+
 export default function ContactForm() {
   return (
       <section>
             <div className="container contact">
                     <h2 className="text--subtitle text--center text--blue"> ¿Quienes somos? </h2>
                     <form action="" method="" className="bg--green form">
-                        <div className="form__group">
+                        <div className="form__group form__group--flex">
                             <div className="form__input">
-                                <label htmlFor="">Nombre:</label>
-                                <input type="text" />
+                                <label htmlFor="" className="text--white">Nombre:</label>
+                                <input type="text" placeholder="Ingresa tu nombre." />
                             </div>
                             <div className="form__input">
-                                <label htmlFor="">Email:</label>
-                                <input type="text" />
-                            </div>
-                        </div>
-                        <div className="form__group">
-                            <div className="form__input">
-                                <label htmlFor="">Asunto:</label>
-                                <input type="text" />
+                                <label htmlFor="" className="text--white">Email:</label>
+                                <input type="text" placeholder="ejemplo@gmail.com"/>
                             </div>
                         </div>
                         <div className="form__group">
                             <div className="form__input">
-                                <label htmlFor="">Mensaje</label>
-                                <textarea > </textarea>
+                                <label htmlFor="" className="text--white">Asunto:</label>
+                                <input type="text" placeholder="Escribe de qué trata tu mensaje" />
                             </div>
                         </div>
+                        <div className="form__group">
+                            <div className="form__input">
+                                <label htmlFor="" className="text--white">Mensaje:</label>
+                                <textarea style={{resize: 'none', height: '100px'}} placeholder="Ingresa el mensaje..."> </textarea>
+                            </div>
+                        </div>
+                        <Button className="button--orange" themeColor="warning" svgIcon={icons.uploadIcon}>
+                            Enviar
+                        </Button>
                     </form>
             </div>
       </section>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import '@progress/kendo-theme-default/dist/all.css';
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "@progress/kendo-theme-default/dist/all.css";
+import "./index.css";
+import "./styles.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
