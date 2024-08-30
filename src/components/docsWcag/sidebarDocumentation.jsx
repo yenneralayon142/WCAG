97f2c21aebbec73f56d6a8ef1420a0546b45dc83
Yenner Alayon <yenneralayon@hotmail.com>
2024-08-30 16:25:51 -0500

    feat(front): Add menú docs

---
+++ src/components/docsWcag/sidebarDocumentation.jsx
@@ -0,0 +1,20 @@
+export default function Sidebar  () {
+    return (
+      <div className="sidebar__Documentation">
+        <h3>Menú</h3>
+        <ul>
+          <li>Introducción</li>
+          <li>Descargar</li>
+          <li>Contenido</li>
+          <li>Navegadores y dispositivos</li>
+          <li>JavaScript</li>
+          <li>Paquete web</li>
+          <li>Vite</li>
+          <li>Accesibilidad</li>
+          <li>RFS</li>
+          <li>RTL</li>
+          <li>Contribuir</li>
+        </ul>
+      </div>
+    );
+  }
