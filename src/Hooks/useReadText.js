a5a640634eb6f67449aa3c795ac1d9d8aee7fd15
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-03 16:31:55 -0500

    refactor: format and edit the code

--- src/Hooks/useReadText.js
+++ src/Hooks/useReadText.js
@@ -9,7 +9,7 @@ export function useReadText() {
             if (typeof speechSynthesis === 'undefined') return;
         
             let voices = [];
-            const textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, label, select, input, button, footer, header, .text--normal, .text--large, .text--extralarge, .text--subtitle, .text-title, .k-menu-link-text');
+            const textElements = document.querySelectorAll('p, span, a, li, td, th, h1, h2, h3, h4, h5, h6, label, select, input, button, footer, header, .text--normal, .text--large, .text--extralarge, .text--subtitle, .text-title, .k-menu-link-text');
         
             const populateVoiceList = () => {
               voices = speechSynthesis.getVoices();
