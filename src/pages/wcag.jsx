4ea6121426308aa9f13854a11d11080b85c461f6
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-04 00:01:43 -0500

    chore: remove disabilities title

--- src/pages/wcag.jsx
+++ src/pages/wcag.jsx
@@ -1,7 +1,6 @@
 import Hero from "../components/wcag/hero";
 import Principles from "../components/wcag/principles";
 import Criteria from "../components/wcag/criteria";
-import Disabilities from "../components/wcag/disabilities";
 
 export default function Wcag() {
     return (
@@ -9,7 +8,6 @@ export default function Wcag() {
             <Hero />
             <Principles />
             <Criteria />
-            <Disabilities />
         </main>
     );
 }
