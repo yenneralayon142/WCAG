c6b63c2886c05a6a7d4018939ef5618355342397
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-23 19:35:28 -0500

    feat: add breadcrumb

--- src/layouts/app.jsx
+++ src/layouts/app.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "./header";
 import Footer from "./footer";
 import AccessibilityControls from "./accesibityControls";
+import BreadCrumb from "../components/breadCrumb";
 
 export default function AppLayout() {
     return (
         <>
             <div id="app__container">
                 <Header />
+                <BreadCrumb />
                 <Outlet />
                 <Footer />
             </div>
--- src/layouts/docs.jsx
+++ src/layouts/docs.jsx
@@ -3,12 +3,14 @@ import Header from "./header";
 import AccessibilityControls from "./accesibityControls";
 import Sidebar from "./sidebarDocumentation";
 import SearchFilters from "./searchFilters";
+import BreadCrumb from "../components/breadCrumb";
 
 export default function AppLayout() {
     return (
         <>
             <div id="docs__container">
                 <Header />
+                <BreadCrumb />
                 <div className="docs__main">
                     <Sidebar />
                     <div className="docs__content">
