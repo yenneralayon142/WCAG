d856e4832615bee692f32cc8e8de401074ed4480
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-24 00:26:43 -0500

    fix: fix  docs search working

--- src/layouts/docs.jsx
+++ src/layouts/docs.jsx
@@ -1,11 +1,50 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./header";
 import AccessibilityControls from "./accesibityControls";
 import Sidebar from "./sidebarDocumentation";
 import SearchFilters from "./searchFilters";
 import BreadCrumb from "../components/breadCrumb";
+import { useEffect } from "react";
 
-export default function AppLayout() {
+export default function DocsLayout() {
+    const location = useLocation();
+
+    // Función para desplazar al elemento deseado
+    const scrollToElementInContainer = () => {
+        const hash = window.location.hash;
+        const container = document.querySelector(".docs__content");
+
+        if (hash.length > 0) {
+            const targetElement = document.querySelector(hash);
+
+            if (targetElement && container) {
+                container.scrollTo({
+                    top: targetElement.offsetTop - container.offsetTop,
+                    behavior: "smooth",
+                });
+            }
+        } else {
+            container.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+    };
+
+    useEffect(() => {
+        scrollToElementInContainer();
+
+        const handleHashChange = () => {
+            scrollToElementInContainer();
+        };
+
+        window.addEventListener("hashchange", handleHashChange);
+
+        return () => {
+            window.removeEventListener("hashchange", handleHashChange);
+        };
+    }, [location.pathname, window.location.hash]);
+
     return (
         <>
             <div id="docs__container">
